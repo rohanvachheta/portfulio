eb5b3bb35a0b3afd1366777c59ceb1071e391849
rohan23 <rohan@socialpilot.co>
2020-01-04 20:23:54 +0530

    added polyfile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "babel-polyfill";
 
 // pages
 import NavBar from "./components/navBar/NavBar";
