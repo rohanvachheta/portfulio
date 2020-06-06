d9cd08bcc95c176e82f235942d2a8fd51a462433
rohan23 <rohan@socialpilot.co>
2020-06-06 10:40:40 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -27,6 +27,41 @@ library.add(faEnvelope, faKey, fas);
 
 function App() {
   useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/custom-service-worker.js")
+        .then((reg) => {
+          if (reg.waiting) {
+            // a new version is already waiting to take control
+            this.newWorker = reg.waiting;
+            window.confirm("new verison bro");
+
+            /*
+              code omitted: displays a snackbar to the user to manually trigger
+              activation of the new SW. This will be done by calling skipWaiting()
+              then reloading the page
+            */
+          }
+
+          // handler for updates occuring while the app is running, either actively or in the background
+          reg.onupdatefound = () => {
+            this.newWorker = reg.installing;
+
+            this.newWorker.onstatechange = () => {
+              if (this.newWorker.state === "installed") {
+                if (reg.active) {
+                  // a version of the SW already has control over the app
+                  /*
+                    same code omitted
+                  */
+                } else {
+                  // very first service worker registration, do nothing
+                }
+              }
+            };
+          };
+        });
+    }
     // window.confirm(`New content is available!. Click OK to refresh`);
   }, []);
 
