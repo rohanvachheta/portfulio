a401604c631b1f38222781c5ab9fcaf0b5f2591b
rohan23 <rohan@socialpilot.co>
2020-06-06 10:02:36 +0530

    update

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -73,6 +73,7 @@ function registerValidSW(swUrl, config) {
                 "New content is available and will be used when all " +
                   "tabs for this page are closed. See https://bit.ly/CRA-PWA."
               );
+              window.alert("update ?");
 
               // Execute callback
               if (config && config.onUpdate) {
