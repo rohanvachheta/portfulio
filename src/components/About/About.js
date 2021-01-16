6384a637fd3dbd0f8b67ca396d14b6c58b2b2a9f
Rohan Vachheta <38291892+rohanvachheta@users.noreply.github.com>
2021-01-16 16:43:28 +0530

    fix usye

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./About.css";
 import portfulio from "./../../assets/portfulio.jpg";
-import LinkEditorExample from "../DraftJsExmp";
 
 function About(props) {
   return (
